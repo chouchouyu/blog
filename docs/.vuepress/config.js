d0114347ef4c719747a7499a0f46433913b19075
susan.wu <18673104340@qq.com>
2021-05-28 16:55:02 +0800

    deploy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: '丑丑鱼的个人博客',
   description: 'susan blog',
   port: '7777',
-  base: "/blog/"
+  base: "/blog/",
   head: [
     ['link', {rel: 'icon', href: '/img/logo.png'}],//地址栏图标
     ['link', {rel: 'stylesheet', href: '/css/style.css'}],
